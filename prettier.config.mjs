08136b2c9e8d1e04b5ba03eb8ec9c92f47f44a6d
Admin AllyDevs <admin@allydevs.com>
2026-08-23 22:44:43 +0530

    chore: add code quality tooling

---
+++ prettier.config.mjs
@@ -0,0 +1,11 @@
+const config = {
+  plugins: ['prettier-plugin-tailwindcss'],
+  singleQuote: true,
+  semi: true,
+  trailingComma: 'es5',
+  printWidth: 100,
+  tabWidth: 2,
+  arrowParens: 'always',
+};
+
+export default config;
